f9edca1a4389475efd1d8c653a3c052abeaf6697
Sujoy Manna <98382232+Sujoy098@users.noreply.github.com>
2025-07-18 16:14:38 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1245;
+let accountgmail="sujoy@123";
+var accountpassword ="123";
+acountCity="kolkata";
+
+//constId = not change 
+
+accountgmail="sujoy@456"
+accountpassword="456"
+acountCity="medinipur"
+
+console.log(accountId);
+console.log(accountgmail);
+
+console.log([accountId,accountgmail,accountpassword,acountCity]);
+
