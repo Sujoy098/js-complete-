6762897190fd1b6f8a8f257df381894809645289
Sujoy Manna <98382232+Sujoy098@users.noreply.github.com>
2025-07-21 14:41:02 +0000

    string add extra methods

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,11 +1,46 @@
-const name = "hitesh"
+const name = "sujoy"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const nameFull = "sujoy"
+let repoCountN = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${nameFull} and my repo count is ${repoCountN}`);
+
+
+repoCountN = repoCountN + 50;
+
+console.log(nameFull + repoCountN + " Value");
+
+
+console.log(`Hello my name is ${nameFull} and my repo count is ${repoCountN}`);
+
+console.log(typeof repoCountN);
+
+let booleanIsLoggedIn = Boolean(repoCountN);
+
+console.log(typeof booleanIsLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+repoCountN = repoCountN / 100;
+
+console.log(booleanIsLoggedIn);
+
+repoCountN = repoCountN / 0;
+
+console.log(booleanIsLoggedIn);
+
+
+console.log(repoCountN);
+
+
+const gameName = new String('sujoy-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +57,13 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "  sujoy    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
+//The trim() method of String values removes whitespace from both ends of this string 
+// and returns a new string, without modifying the original string.
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://sujoy.com/sujoymanna%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
