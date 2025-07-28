a59b965fb9a02696c43d2a30e344873d77cd50e3
Sujoy Manna <98382232+Sujoy098@users.noreply.github.com>
2025-07-28 14:20:00 +0000

    array prototype map

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -8,3 +8,29 @@ const newNums = myNumers
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
+
+//The Map object holds key-value pairs and remembers the original insertion order of the keys.
+//  Any value (both objects and primitive values) may be used as either a key or a value.
+
+const map = new Map();
+
+map.set("a", 1);
+map.set("b", 2);
+map.set("c", 3);
+
+console.log(map.get("a"));
+// Expected output: 1
+
+map.set("a", 97);
+
+console.log(map.get("a"));
+// Expected output: 97
+
+console.log(map.size);
+// Expected output: 3
+
+map.delete("b");
+
+console.log(map.size);
+// Expected output: 2
+console.log(map.get("b"));
